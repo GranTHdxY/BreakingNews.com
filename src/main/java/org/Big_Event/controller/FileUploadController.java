3b6cdb8a1c79cfcb79e4092e5521c5049ea1a0ec
GranTHdxY <1021734560@qq.com>
2024-05-11 16:57:37 +0800

    更新了文件上传接口

--- src/main/java/org/Big_Event/controller/FileUploadController.java
+++ src/main/java/org/Big_Event/controller/FileUploadController.java
@@ -9,6 +9,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.UUID;
 
 @RestController
 @Validated
@@ -18,7 +19,12 @@ public class FileUploadController {
     public Result<String> upload(MultipartFile file) throws IOException {
         //把文件的内容存到本地磁盘上？？
         String originalFilename = file.getOriginalFilename();
+        //保证文件名字唯一，防止文件覆盖
+        //在文件名后拼上uuid字符串
+        String filename = UUID.randomUUID().toString() + originalFilename.substring(originalFilename.lastIndexOf("."));
+
         file.transferTo(new File("D:/JavaProjects/LearningMaterials/files" + originalFilename));
+
         //不能直接传入success:应该是访问在三方服务器上的url地址 而不是访问本地磁盘的url地址
         return Result.success("服务器上的url地址");
 
